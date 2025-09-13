03d2ba73b22d76dc6ae8c76283e2d3dba1117832
yousseffjoseph7585-cell <yousseffjoseph7585@gmail.com>
2025-09-13 20:10:10 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+nom = "Dosso"
+age = 22
+taille = 1.75
+Est_etudiant = true
+Est_etudiant = false
+print(f"Je m'appelle {nom}, j'ai {age} ans et je mesure {taille} M. Je suis {Est_etudiant}" )
+type(nom)
+type(age)
+type(taille)
+type(Est_etudiant)
