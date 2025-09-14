60ef4f9127a4009f504a6d54db92b0b46da8122a
yousseffjoseph7585-cell <yousseffjoseph7585@gmail.com>
2025-09-14 22:38:09 +0000

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,15 @@
 # Écrivez votre code ici !
+# Crée un nom
+Nom = "Dosso"
+
+# Crée un age
+age = 25
+
+# Affichage
+print(f"je m'appelle {Nom} et j'ai {age} ans")
+
+# Modification
+age_2 = "age + 10"
+
+# New affichage
+print(f"je m'appelle {nom} et j'ai {age_2} ans")
