5d23e97b3a64339babf3fb9b6263b51e273e1fbd
yousseffjoseph7585-cell <yousseffjoseph7585@gmail.com>
2025-09-14 22:12:49 +0000

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,17 @@
-# Écrivez votre code ici !
+# Creation d'un dictionnaire
+Fruits = {"pomme" : "rouge", "banane" : "jaune", "orange" : "orange"}
+
+# Ajouter la clé kiwi
+Fruits['kiwi'] = "vert"
+
+# Acceder a la valeur Banane et stocker dans une variable_couleur
+variable_couleur = Fruits["banane"]
+
+# Modification
+Fruits["pomme"] = "vert"
+
+# Suppression 
+del Fruits["banane"]
+
+# Affichage des clés restantes
+print(Fruits.key())
