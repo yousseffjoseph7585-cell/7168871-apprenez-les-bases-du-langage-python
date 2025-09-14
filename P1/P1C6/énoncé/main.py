6d8e6491cb104ffc6785a14f2fe0be832a64b86a
yousseffjoseph7585-cell <yousseffjoseph7585@gmail.com>
2025-09-14 11:51:57 +0000

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+Mes_fruits = ["pomme", "banane", "orange"]
+Mes_fruits.append("kiwi")
+Mes_fruits.remove("orange")
+Mes_fruits [2] = ananas
+len(Mes_fruits)
+Mes_fruits.sort()
