bbbf7af0cc8a2cba0829e1897ab3fce4d89ab704
yousseffjoseph7585-cell <yousseffjoseph7585@gmail.com>
2025-10-05 12:35:49 +0000

    Update main.py
    Calcul avec les listes

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,43 @@
-# Ecrivez votre code ici !
+# Saisir une liste de nombres séparés par des virgules
+nombre = input("Saisir une liste de nombres séparés par des virgules")
+
+# Séparer l'ensemble des nombres et les insérer dans une liste
+liste = nombres.split(",")
+
+# Affichage
+print("Liste des nombre: ", liste )
+
+# Liste est une chaîne de caractère
+# Convertir les elements de la liste en enier
+liste_entiers = []
+for nombre in liste:
+  nombre_entier = int(nombre)
+  liste_entiers.append(nombre_entier)
+
+# calculez la somme des entiers
+somme = 0
+for nombre in liste_entiers:
+  somme += nombre 
+
+# Equivalent à:
+# somme
+print("Somme des nombres:", somme)
+
+# effectuez la moyenne
+moyenne = somme / len(liste_entiers)
+print("moyenne des nombres:", moyenne)
+
+# Trouvez combien de nombre de la liste sont superieurs à la moyenne
+nombre_au_dessus_moyenne = 
+for nombre in liste_entiers:
+  if nombre > moyenne:
+    nombre_au_dessus_moyenne += 
+print("Nombre de nombres superieurs à la moyenne:", nombre_au_dessus_moyenne)
+
+# Nombre
+nombres_pairs = 
+for nombre in liste_entiers:
+  if nombre % 2 ==  0:
+    nombres_pairs = nombres_pairs + 1
+
+print("Nombre de nombre pairs:", nombres_pairs)
