7fc08bde45b8b8223eb6bc036386739ea561dbea
yousseffjoseph7585-cell <yousseffjoseph7585@gmail.com>
2025-10-24 10:20:09 +0000

    Implement addition and multiplication functions
    Added functions for addition and multiplication.

--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+# Fichier operations.py
+
+# Fontion adition
+def addition(a, b):
+  return a + b
+# Fonction multiplication
+def multiplication(a, b):
+  return a * b
+
+
